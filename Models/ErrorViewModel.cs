1756285d067f2a70a3c127885044d5b3b298c8a5
Burcinbkn <111539058+Burcinbkn@users.noreply.github.com>
2022-11-13 18:04:34 +0300

    adding existing project files to git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BurcinBakinerBlog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
